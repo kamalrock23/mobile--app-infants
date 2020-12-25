b6a9670b79eeda4de666e57a0414286f2f4035ac
Kamal Kishore <kamal@mds.asia>
2020-12-26 03:09:38 +0530

    scrolling design fix.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,3 +13,13 @@ $(window).bind("load resize", function() {
 
 function sizingheight(){
 }
+
+$('.fullheight__body').scroll(function() {    
+    var scroll = $('.fullheight__body').scrollTop();
+    if (scroll >= 54) {
+        $(".fullheight__header").addClass("page-scrolled");
+	}
+	else {
+		$(".fullheight__header").removeClass("page-scrolled");
+	}
+}); //missing );
